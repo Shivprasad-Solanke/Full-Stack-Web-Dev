80e0e63464d2d757dfc52f27b5f4aa48e72be34b
Shivprasad-Solanke <shivsolanke12@gmail.com>
2025-03-09 17:11:00 +0530

    started JS Tutorial

---
+++ JS/tutorial/alert.js
@@ -0,0 +1,46 @@
+"use strict"
+
+// single line comment
+
+/* 
+    Multi
+    Line
+    Comment 
+*/
+
+// alert statements
+// alert("External Alert")
+// alert("External Alert")
+
+// "use strict" for modern JS
+"use strict"
+
+// Variables
+// ==================================
+// using let
+// let $_firstName = "Shivprasad";
+// console.log($_firstName);
+// using const
+// const COLOR_RED = "#fff";
+// console.log(COLOR_RED);
+
+// HW
+// =====
+// 1
+// let name = "John";
+// let admin = name;
+// alert(admin);
+
+// 2
+// let ourPlanetName = "Earth";
+// let currentUserName = "Shiv";
+
+// 3
+// let BIRTHDAY = "01.01.2001";
+// let age = someFunction(BIRTHDAY);
+
+
+
+
+
+
