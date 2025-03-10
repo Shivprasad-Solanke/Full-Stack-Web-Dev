960a8b9cab6ff1545bf45b7fa964e0563470e284
Shivprasad-Solanke <shivsolanke12@gmail.com>
2025-03-10 15:38:42 +0530

    Learned Data Types and Interactions in JS

--- JS/tutorial/alert.js
+++ JS/tutorial/alert.js
@@ -13,7 +13,7 @@
 // alert("External Alert")
 
 // "use strict" for modern JS
-"use strict"
+// "use strict"
 
 // Variables
 // ==================================
@@ -38,6 +38,92 @@
 // 3
 // let BIRTHDAY = "01.01.2001";
 // let age = someFunction(BIRTHDAY);
+
+
+// DATA TYPES
+// =======================================
+
+// 1. Primitive Data Types
+// ===============================
+
+// Dynamically Typed
+// Number
+// =====================
+// undefined
+// let anyDataTypeVar;
+// console.log(typeof(anyDataTypeVar));
+// console.log(anyDataTypeVar);
+
+// // number
+// anyDataTypeVar = 5;
+// console.log(typeof(anyDataTypeVar));
+// console.log(anyDataTypeVar);
+
+// anyDataTypeVar = 9007199254740991 + 2;
+// console.log(anyDataTypeVar);
+
+// anyDataTypeVar = 4.323;
+// console.log(typeof anyDataTypeVar);
+// console.log(anyDataTypeVar);
+
+// // BigInt
+// anyDataTypeVar = 1234567890123456789012345678901234567890n;
+// console.log(typeof(anyDataTypeVar));
+// console.log(anyDataTypeVar);
+
+// // NaN Not a Number
+// anyDataTypeVar = NaN;
+// console.log(anyDataTypeVar);
+
+// anyDataTypeVar = "string divided by Number" / 10;
+// console.log(anyDataTypeVar);
+
+// // string
+// anyDataTypeVar = "This is a Double Quoted String ! ";
+// console.log(anyDataTypeVar);
+
+// anyDataTypeVar = 'This is a Single Quoted String !';
+// console.log(anyDataTypeVar);
+
+// anyDataTypeVar = `This is a Back-Ticked Quote for using expression and Vars inside a String like this 5 + 4 = ${5 + 4}`;
+// console.log(anyDataTypeVar)
+
+// // boolean
+// anyDataTypeVar = true;
+// console.log(anyDataTypeVar);
+
+// anyDataTypeVar = 5 > 2;
+// console.log(anyDataTypeVar);
+
+// // null
+// anyDataTypeVar = null;
+// console.log(anyDataTypeVar);
+
+// // typeof
+// console.log("Type of shows type of any data !");
+// console.log(`eg. typeof 45.34 = ${typeof 45.34}`);
+
+// // Interactions : ALERT, PROMPT, CONFIRM
+// // =========================================
+
+// // alert
+// alert("This is a alert !");
+
+// // prompt
+// let promptResult = prompt("Enter your Age :", 99);
+// alert(`Your age is ${promptResult}`);
+
+// // confirm
+// let confirmResult = confirm("Click OK for true and Cancel for false, Ok ?");
+// alert(`You chose ${confirmResult}`);
+
+// // *** Task
+// let userName = prompt("What is your name ?");
+// alert(`Your name is ${userName}`);
+
+
+
+
 
 
 
